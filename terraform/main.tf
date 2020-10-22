8de5206a68628d0a2c3566848718cbbf8ec73443
Akshay Kumar Shreekanth <shreekan@usc.edu>
2020-10-22 15:25:36 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,6 @@
 terraform {
   required_providers {
-    mycloud = {
-      source  = "mycorp/mycloud"
-      version = "~> 1.0"
-    }
+    aws = "~> 1.0"
   }
-}
-
-resource "null_resource" "dummy" {
 }
 
